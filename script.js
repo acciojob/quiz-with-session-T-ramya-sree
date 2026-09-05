276f5661d59c8b0273fe73946d601dca0315adf4
T-ramya-sree <85395315+T-ramya-sree@users.noreply.github.com>
2026-09-05 13:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-//your JS code here.
+// your JS code here.
+
+const questionsElement = document.getElementById("questions");
+const scoreElement = document.getElementById("score");
+const submitButton = document.getElementById("submit");
+
+// Load saved answers from sessionStorage
+let userAnswers =
+  JSON.parse(sessionStorage.getItem("progress")) || {};
 
 // Do not change code below this line
 // This code will just display the questions to the screen
@@ -33,24 +41,3 @@ const questions = [
 // Display the quiz questions and choices
 function renderQuestions() {
   for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
-    }
-    questionsElement.appendChild(questionElement);
-  }
-}
-renderQuestions();
